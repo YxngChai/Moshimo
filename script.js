6bf8868eb125fc3d454aa2f2ccdf7550add8206f
Valentin Robert <vrobert.fr@gmail.com>
2026-02-13 18:15:01 +0100

    13/2/26

--- script.js
+++ script.js
@@ -46,21 +46,16 @@ if (isTouch) {
   });
 }
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry.target);
-        entry.target.classList.add("visible");
-      } else {
-        entry.target.classList.remove("visible");
-      }
-    });
-  },
-  {
-    threshold: 1,
-  },
-);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+}, {});
 const moshiElements = document.querySelectorAll(".slide");
 
 moshiElements.forEach((el) => observer.observe(el));
